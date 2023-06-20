7ad17fd5a15c830d7517fa14d9ab151eb28637e6
AsgerJon <asgerjon2@gmail.com>
2023-06-20 03:42:18 +0200

    initial commit

--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Import order"""
-#  MIT Licence
-#  Copyright (c) 2023 Asger Jon Vistisen
-from __future__ import annotations
-
-from icecream import ic
-
-ic.configureOutput(includeContext=True)
--- src/chessgpt/__init__.py
+++
@@ -1,5 +0,0 @@
-"""The chessgpt contains the logic used by ChessGPT to analyze positions
-and to play chess"""
-#  MIT Licence
-#  Copyright (c) 2023 Asger Jon Vistisen
-from __future__ import annotations
